ce114fe29baa46d72c1aec99c9bdb47fcfdfe1f6
Byron Hall <byronhall020@gmail.com>
2019-12-02 15:33:57 -0600

    dec 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btcAddress: "",
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  render() {
+    return (
+      <>
+        <input
+          id="btcAddress"
+          type="text"
+          value={this.state.btcAddress}
+          placeholder="btc address"
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+        <div>
+          <span id="stuff"></span>
+        </div>
+      </>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        // Typical action to be performed when the document is ready:
+        // document.getElementById("demo").innerHTML = xhttp.responseText;
+        console.log(xhttp.responseText);
+      }
+    };
+    xhttp.open("GET", "https://www.blockchain.com/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6", true);
+    xhttp.send();
+  }
 }
 
 export default App;
